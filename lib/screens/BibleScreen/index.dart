894e37a103bfe44b03f0cba66f988633bb643709
Dagmawi Nebiat <dagmnebiat@gmail.com>
2024-08-03 17:44:31 +0300

    Chapter page refactored to its own widget

--- lib/screens/BibleScreen/index.dart
+++ lib/screens/BibleScreen/index.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:living_way/controllers/content_controller.dart';
-import 'package:living_way/controllers/layout_controller.dart';
 import 'package:living_way/screens/BibleScreen/widgets/bible_navigator.dart';
+import 'package:living_way/screens/BibleScreen/widgets/chapter_page.dart';
 import 'package:living_way/screens/BibleScreen/widgets/translation_popup_button.dart';
 import 'package:living_way/themes/light_theme.dart';
 import 'package:provider/provider.dart';
@@ -18,29 +18,17 @@ class _BibleScreenState extends State<BibleScreen>
   final scrollController = ScrollController();
   late AnimationController animationController = AnimationController(
       vsync: this, duration: const Duration(milliseconds: 300));
-  late AnimationController verseHighlightController = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 300),
-      lowerBound: 0.3,
-      value: 1);
   bool isBibleNavigatorVisible = false;
 
   @override
   Widget build(BuildContext context) {
     final contentController = Provider.of<ContentController>(context);
-    final layoutController = Provider.of<LayoutController>(context);
     final selectedBook =
         contentController.book ?? contentController.bible.firstOrNull;
     final selectedChapter = contentController.chapter ?? 0;
-    final selectedVerse = contentController.verse;
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
     Orientation orientation = MediaQuery.of(context).orientation;
-    List<GlobalKey> verseKeys = List.generate(
-        selectedBook?.chapters[selectedChapter].length ?? 0,
-        (index) => GlobalKey());
-    layoutController.setVerseKeys = verseKeys;
-    layoutController.setVerseAnimationController = verseHighlightController;
 
     return SafeArea(
         child: contentController.bible.isNotEmpty
@@ -105,57 +93,7 @@ class _BibleScreenState extends State<BibleScreen>
                           padding: const EdgeInsets.all(10),
                           child: const BibleNavigator());
                     }),
-                Expanded(
-                    child: Container(
-                        margin: const EdgeInsets.all(20),
-                        child: SingleChildScrollView(
-                            controller: layoutController.scrollController,
-                            child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children:
-                                    (selectedBook?.chapters[selectedChapter] ??
-                                            [])
-                                        .map((verse) {
-                                  final index = (selectedBook
-                                              ?.chapters[selectedChapter] ??
-                                          [])
-                                      .indexOf(verse);
-
-                                  return AnimatedBuilder(
-                                      animation: verseHighlightController,
-                                      builder: (context, child) {
-                                        final isSelectedVerse =
-                                            selectedVerse == index;
-
-                                        return Container(
-                                            margin: const EdgeInsets.symmetric(
-                                                vertical: 10),
-                                            child: RichText(
-                                                key: verseKeys[index],
-                                                text: TextSpan(children: [
-                                                  WidgetSpan(
-                                                      child: Text(
-                                                          '${index + 1} ',
-                                                          style: TextStyle(
-                                                              color: isSelectedVerse
-                                                                  ? lightPrimaryColor
-                                                                  : lightPrimaryColor
-                                                                      .withOpacity(
-                                                                          verseHighlightController
-                                                                              .value),
-                                                              fontSize: 24))),
-                                                  TextSpan(
-                                                      text: verse,
-                                                      style: TextStyle(
-                                                          color: isSelectedVerse
-                                                              ? Colors.black
-                                                              : Colors.black
-                                                                  .withOpacity(
-                                                                      verseHighlightController
-                                                                          .value)))
-                                                ])));
-                                      });
-                                }).toList()))))
+                    ChapterPage(verses: (selectedBook?.chapters[selectedChapter] ?? []))
               ])
             : const Center(
                 child: CircularProgressIndicator(color: lightPrimaryColor)));
---
+++ lib/screens/BibleScreen/widgets/chapter_page.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:living_way/controllers/content_controller.dart';
+import 'package:living_way/controllers/layout_controller.dart';
+import 'package:living_way/themes/light_theme.dart';
+import 'package:provider/provider.dart';
+
+class ChapterPage extends StatefulWidget {
+  final List<String> verses;
+  const ChapterPage({super.key, required this.verses});
+
+  @override
+  State<ChapterPage> createState() => _ChapterPageState();
+}
+
+class _ChapterPageState extends State<ChapterPage>
+    with TickerProviderStateMixin {
+  late AnimationController verseHighlightController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 250),
+      lowerBound: 0.3,
+      value: 1);
+
+  @override
+  Widget build(BuildContext context) {
+    final contentController = Provider.of<ContentController>(context);
+    final layoutController = Provider.of<LayoutController>(context);
+    final selectedVerse = contentController.verse;
+    final selectedBook =
+        contentController.book ?? contentController.bible.firstOrNull;
+    final selectedChapter = contentController.chapter ?? 0;
+
+    List<GlobalKey> verseKeys = List.generate(
+        selectedBook?.chapters[selectedChapter].length ?? 0,
+        (index) => GlobalKey());
+    layoutController.setVerseKeys = verseKeys;
+    layoutController.setVerseAnimationController = verseHighlightController;
+
+    return Expanded(
+        child: Container(
+            margin: const EdgeInsets.all(20),
+            child: SingleChildScrollView(
+                controller: layoutController.scrollController,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: widget.verses.map((verse) {
+                      final index = widget.verses.indexOf(verse);
+                      
+                      return AnimatedBuilder(
+                          animation: verseHighlightController,
+                          builder: (context, child) {
+                            final isSelectedVerse = selectedVerse == index;
+
+                            return Container(
+                                margin:
+                                    const EdgeInsets.symmetric(vertical: 10),
+                                child: RichText(
+                                    key: verseKeys[index],
+                                    text: TextSpan(children: [
+                                      TextSpan(
+                                          text: '${index + 1} ',
+                                          style: TextStyle(
+                                              color: isSelectedVerse
+                                                  ? lightPrimaryColor
+                                                  : lightPrimaryColor.withOpacity(
+                                                      verseHighlightController
+                                                          .value),
+                                              fontSize: 24)),
+                                      TextSpan(
+                                          text: verse,
+                                          style: TextStyle(
+                                              color: isSelectedVerse
+                                                  ? Colors.black
+                                                  : Colors.black.withOpacity(
+                                                      verseHighlightController
+                                                          .value)))
+                                    ])));
+                          });
+                    }).toList()))));
+  }
+}
