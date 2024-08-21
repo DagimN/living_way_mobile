b930d1505d7665649b1a55ca7ec22b7852672a4a
Dagmawi Nebiat <dagmnebiat@gmail.com>
2024-08-21 18:52:53 +0300

    Layout issue fixed in profile screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -14,54 +14,65 @@ class ProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     const double imageSize = 128.0;
     final profileController = Provider.of<ProfileController>(context);
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+    Orientation orientation = MediaQuery.of(context).orientation;
     final settingsNavigation =
         Provider.of<LayoutController>(context).settingsNavigation;
     final profile = profileController.userProfile ??
         Profile(firstName: "John", lastName: "Doe");
-    final imageProvider =
-        profile.imageUrl != null ? CachedNetworkImageProvider(profile.imageUrl!) : null;
+    final imageProvider = profile.imageUrl != null
+        ? CachedNetworkImageProvider(profile.imageUrl!)
+        : null;
 
     return SafeArea(
-        child: Column(children: [
-      Align(
-          alignment: Alignment.topRight,
-          child: IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.notifications_none_rounded,
-                  color: lightPrimaryColor))),
-      Container(
-          height: imageSize,
-          width: imageSize,
-          margin: const EdgeInsets.all(14),
-          decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              boxShadow: [
-                BoxShadow(
-                    color: lightPrimaryColor.withOpacity(.2),
-                    offset: const Offset(0, 7),
-                    blurRadius: 21,
-                    spreadRadius: 1)
-              ],
-              image: DecorationImage(
-                  image: profile.imageUrl != null
-                      ? imageProvider!
-                      : Image.asset(AppImages.profilePlaceholder).image))),
-      Text('${profile.firstName} ${profile.lastName}',
-          style: const TextStyle(fontSize: 24, fontWeight: FontWeight.w400)),
-      const SizedBox(height: 30),
-      Expanded(
-          child: ListView.builder(
-              shrinkWrap: true,
-              itemCount: settingsNavigation.length,
-              itemBuilder: (context, index) {
-                final navigationItem = settingsNavigation[index];
-                return ListTile(
-                    onTap: () => Navigator.pushNamed(
-                        context, navigationItem['route'] ?? ''),
-                    title: Text(navigationItem['name'] ?? ''),
-                    trailing:
-                        const Icon(Icons.arrow_forward_ios_rounded, size: 14));
-              }))
-    ]));
+        child: SingleChildScrollView(
+            child: SizedBox(
+      height: orientation == Orientation.portrait
+          ? screenHeight * .8
+          : screenWidth * .6,
+      child: Column(mainAxisSize: MainAxisSize.min, children: [
+        Align(
+            alignment: Alignment.topRight,
+            child: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.notifications_none_rounded,
+                    color: lightPrimaryColor))),
+        Container(
+            height: imageSize,
+            width: imageSize,
+            margin: const EdgeInsets.all(14),
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                      color: lightPrimaryColor.withOpacity(.2),
+                      offset: const Offset(0, 7),
+                      blurRadius: 21,
+                      spreadRadius: 1)
+                ],
+                image: DecorationImage(
+                    image: profile.imageUrl != null
+                        ? imageProvider!
+                        : Image.asset(AppImages.profilePlaceholder).image))),
+        Text('${profile.firstName} ${profile.lastName}',
+            style: const TextStyle(fontSize: 24, fontWeight: FontWeight.w400)),
+        const SizedBox(height: 30),
+        Expanded(
+            child: ListView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: settingsNavigation.length,
+                itemBuilder: (context, index) {
+                  final navigationItem = settingsNavigation[index];
+                  return ListTile(
+                      onTap: () => Navigator.pushNamed(
+                          context, navigationItem['route'] ?? ''),
+                      title: Text(navigationItem['name'] ?? ''),
+                      trailing: const Icon(Icons.arrow_forward_ios_rounded,
+                          size: 14));
+                }))
+      ]),
+    )));
   }
 }
