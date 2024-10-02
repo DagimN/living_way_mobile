d0bcf7ecdc704ba08700a4832c5223cce2023edb
Dagmawi Nebiat <dagmnebiat@gmail.com>
2024-10-02 15:33:07 +0300

    WIP on main: 6036218 changed last scroll item text in activities screen

--- lib/app.dart
+++ lib/app.dart
@@ -3,6 +3,7 @@ import 'package:living_way/controllers/auth_controller.dart';
 import 'package:living_way/controllers/content_controller.dart';
 import 'package:living_way/controllers/layout_controller.dart';
 import 'package:living_way/controllers/profile_controller.dart';
+import 'package:living_way/screens/AuthScreens/login_screen.dart';
 import 'package:living_way/screens/IntroScreens/intro_screen.dart';
 import 'package:living_way/screens/IntroScreens/splash_screen.dart';
 import 'package:living_way/screens/Settings/about_screen.dart';
@@ -32,27 +33,25 @@ class LivingWayApp extends StatelessWidget {
           ChangeNotifierProvider<ProfileController>(
               create: (_) => ProfileController())
         ],
-        child: 
-          Consumer<AuthController>(
-            builder: (context, authController, _) {
-              return MaterialApp(
-                  debugShowCheckedModeBanner: false,
-                  title: 'Living Way',
-                  theme: lightTheme,
-                  darkTheme: darkTheme,
-                  initialRoute: authController.isLoggedIn ? '/home' : '/intro',
-                  routes: {
-                    '/splash': (context) => SplashScreen(context),
-                    '/intro': (context) => const IntroScreen(),
-                    '/home': (context) => const HomeScreen(),
-                    '/search': (context) => const SearchScreen(),
-                    '/settings': (context) => const SettingsScreen(),
-                    '/profile': (context) => const ProfileSettingsScreen(),
-                    '/contacts': (context) => const ContactsScreen(),
-                    '/about': (context) => const AboutScreen(),
-                    '/donation': (context) => const DonationScreen()
-                  });
-            }
-          ));
+        child: Consumer<AuthController>(builder: (context, authController, _) {
+          return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Living Way',
+              theme: lightTheme,
+              darkTheme: darkTheme,
+              initialRoute: authController.isLoggedIn ? '/home' : '/login',
+              routes: {
+                '/splash': (context) => SplashScreen(context),
+                '/intro': (context) => const IntroScreen(),
+                '/login': (context) => const LoginScreen(),
+                '/home': (context) => const HomeScreen(),
+                '/search': (context) => const SearchScreen(),
+                '/settings': (context) => const SettingsScreen(),
+                '/profile': (context) => const ProfileSettingsScreen(),
+                '/contacts': (context) => const ContactsScreen(),
+                '/about': (context) => const AboutScreen(),
+                '/donation': (context) => const DonationScreen()
+              });
+        }));
   }
 }
