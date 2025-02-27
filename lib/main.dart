bee07e6c59234dc79bd33e92a87f49a21c21bb23
Dagmawi Nebiat <dagmnebiat@gmail.com>
2025-02-27 20:46:12 +0300

    firebase crashlytics implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
+import 'dart:ui';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart'; 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:living_way/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load();
+
+  await Firebase.initializeApp();
+
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
 
   runApp(const LivingWayApp());
   //TODO: Create a widget that can work as a base screen
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import audioplayers_darwin
+import firebase_analytics
+import firebase_core
+import firebase_crashlytics
 import google_sign_in_ios
 import package_info_plus
 import path_provider_foundation
@@ -15,6 +18,9 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
